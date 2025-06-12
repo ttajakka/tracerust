0a76a82758f778091363323fda5a493cd2c4070d
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-06-12 21:15:22 +0300

    create util module containing PPM struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod color;
 pub mod ray;
+pub mod util;
 pub mod vec3;
---
+++ src/util.rs
@@ -0,0 +1,85 @@
+use crate::color::Color;
+use std::io::{BufWriter, Write};
+
+pub struct PPM {
+    width: u32,
+    height: u32,
+    pixels: Vec<Color>,
+}
+
+impl PPM {
+    pub fn new(width: u32, height: u32) -> Self {
+        PPM {
+            width,
+            height,
+            pixels: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, color: Color) {
+        self.pixels.push(color);
+    }
+
+    pub fn write_to_buffer<W: Write>(&self, writer: &mut BufWriter<W>) {
+        let w = self.width;
+        let h = self.height;
+        writer
+            .write(format!("P3\n{} {}\n255\n", w, h).as_bytes())
+            .unwrap();
+
+        for i in 0..h {
+            eprint!("\rScanlines remaining: {} ", (h - i));
+            for j in 0..w {
+                self.pixels[(i * w + j) as usize].write_io(writer);
+            }
+        }
+        eprint!("\rDone.                   \n");
+    }
+}
+
+pub fn image_test() {
+    let image_width = 256;
+    let image_height = 256;
+
+    let mut ppm = PPM::new(image_width, image_height);
+
+    for i in 0..image_height {
+        for j in 0..image_width {
+            let color = Color::new(
+                0.0_f64,
+                (i as f64) / ((image_width - 1) as f64),
+                (j as f64) / ((image_height - 1) as f64),
+            );
+            ppm.push(color);
+        }
+    }
+
+    let mut stdout = std::io::BufWriter::new(std::io::stdout());
+    ppm.write_to_buffer(&mut stdout);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ppm_writer_works() {
+        let ppm = PPM {
+            width: 2,
+            height: 2,
+            pixels: vec![
+                Color::new(0.0, 1.0, 0.0),
+                Color::new(0.0, 0.5, 0.0),
+                Color::new(0.0, 0.0, 0.0),
+                Color::new(0.0, 0.0, 0.0),
+            ],
+        };
+
+        let actual: Vec<u8> = vec![];
+        let mut writer = BufWriter::new(actual);
+        ppm.write_to_buffer(&mut writer);
+
+        let expected = "P3\n2 2\n255\n0 255 0\n0 127 0\n0 0 0\n0 0 0\n".as_bytes();
+        assert_eq!(writer.buffer(), expected)
+    }
+}
