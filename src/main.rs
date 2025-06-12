38eab8d710c89a7163cebda29bacdfb5dee09598
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-06-12 22:21:19 +0300

    turn main into a simple ray tracer

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-use tracerust::color::Color;
+use std::io::BufWriter;
+
+use tracerust::ray::Ray;
+use tracerust::util::PPM;
+use tracerust::vec3::Vec3;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Choose aspect ratio and image width
+    let aspect_ratio = 16.0_f64 / 9.0_f64;
+    let image_width: u32 = 400;
 
-    let mut stdout = std::io::stdout();
-    print!("P3\n{} {}\n255\n", image_width, image_height);
+    // Calculate the image height, and ensure that it's at least 1.
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
 
-    for i in 0..image_height {
-        eprint!("\rScanlines remaining: {} ", (image_height - i));
-        for j in 0..image_width {
-            let color = Color::new(
-                (i as f64) / ((image_width - 1) as f64),
-                (j as f64) / ((image_height - 1) as f64),
-                0.0_f64,
-            );
-            color.write_io(&mut stdout);
+    // Camera
+    // Note: wiewport widths less than 1 are ok since they are real-valued.
+    let focal_length = 1.0_f64;
+    let viewport_height = 2.0_f64;
+    let viewport_width = viewport_height * (image_width as f64) / (image_height as f64);
+    let camera_center = Vec3(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vec3(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    let mut ppm = PPM::new(image_width, image_height);
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let pixel_center =
+                pixel00_loc + (i as f64) * pixel_delta_u + (j as f64) * pixel_delta_v;
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray::new(camera_center, ray_direction);
+
+            ppm.push(ray.color());
         }
     }
-    eprint!("\rDone.                   \n");
+
+    ppm.write_to_buffer(&mut BufWriter::new(std::io::stdout()));
 }
