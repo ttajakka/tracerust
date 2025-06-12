cd0b26961103573e6b2ac0d850c5c419b5dfc3ef
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-06-12 22:20:44 +0300

    add function to check if ray hits sphere, color accordingly

--- src/ray.rs
+++ src/ray.rs
@@ -1,11 +1,19 @@
-use crate::vec3::Vec3;
+use crate::{color::Color, sphere::Sphere, vec3::Vec3};
 
+#[derive(Debug, Clone, Copy)]
 pub struct Ray {
     origin: Vec3,
     dir: Vec3,
 }
 
 impl Ray {
+    pub fn new(origin: Vec3, dir: Vec3) -> Self {
+        Ray {
+            origin: origin,
+            dir,
+        }
+    }
+
     pub fn origin(&self) -> &Vec3 {
         &self.origin
     }
@@ -16,6 +24,24 @@ impl Ray {
 
     pub fn at(&self, t: f64) -> Vec3 {
         self.origin.clone() + self.dir.clone() * t
+    }
+
+    pub fn hit_sphere(&self, sphere: Sphere) -> bool {
+        let oc = sphere.center() - self.origin;
+        let a = self.dir.dot(self.dir);
+        let b = -2.0 * self.dir.dot(oc);
+        let c = oc.dot(oc) - sphere.radius() * sphere.radius();
+        // Check if the quadratic has solutions
+        b * b - 4.0 * a * c >= 0.0
+    }
+
+    pub fn color(&self) -> Color {
+        if self.hit_sphere(Sphere::new(Vec3(0.0,0.0,-1.0), 0.5)) {
+            return Color::new(1.0, 0.0,0.0)
+        }
+        let u = self.dir.unit();
+        let a = 0.5 * (u.y() + 1.0);
+        (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
     }
 }
 
