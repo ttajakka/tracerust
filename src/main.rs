478f9108e2ae3ab403737beeba20e14613eaafa5
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-06-16 23:08:08 +0300

    final render: adjust camera, add ground and central balls

--- src/main.rs
+++ src/main.rs
@@ -7,51 +7,36 @@ use tracerust::material::{Dielectric, Lambertian, Material, Metal};
 use tracerust::vec3::Vec3;
 
 fn main() {
-    let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
-    let material_bubble: Rc<dyn Material> = Rc::new(Dielectric::new(1.0 / 1.5));
-    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.5));
-
     let mut world = HittableList { objects: vec![] };
+
+    let ground_material: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
     world.add(Rc::new(Sphere::new(
-        Vec3(0., -100.5, -1.),
-        100.,
-        &material_ground,
+        Vec3(0., -1000., 0.),
+        1000.,
+        &ground_material,
     )));
-    world.add(Rc::new(Sphere::new(
-        Vec3(0., 0., -1.2),
-        0.5,
-        &material_center,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Vec3(-1., 0., -1.),
-        0.5,
-        &material_left,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Vec3(-1., 0., -1.),
-        0.4,
-        &material_bubble,
-    )));
-    world.add(Rc::new(Sphere::new(
-        Vec3(1., 0., -1.),
-        0.5,
-        &material_right,
-    )));
+
+    let material_1: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
+    world.add(Rc::new(Sphere::new(Vec3(0., 1., 0.), 1., &material_1)));
+
+    let material_2: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Rc::new(Sphere::new(Vec3(-4., 1., 0.), 1., &material_2)));
+
+    let material_3: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.));
+    world.add(Rc::new(Sphere::new(Vec3(4., 1., 0.), 1.0, &material_3)));
 
     // Set up camera
     let aspect_ratio = 16.0_f64 / 9.0_f64;
-    let image_width = 800;
-    let samples_per_pixel = 100;
-    let max_depth = 50;
+    let image_width = 200;
+    let samples_per_pixel = 10;
+    let max_depth = 10;
 
     let vfov = 20.;
-    let lookfrom = Vec3(-2., 2., 1.);
-    let lookat = Vec3(0., 0., -1.);
+    let lookfrom = Vec3(13., 2., 3.);
+    let lookat = Vec3(0., 0., 0.);
     let vup = Vec3(0., 1., 0.);
-    let defocus_angle = 5.;
-    let focus_distance = 3.4;
+    let defocus_angle = 0.6;
+    let focus_distance = 10.0;
 
     let cam = Camera::new(
         aspect_ratio,
@@ -63,7 +48,7 @@ fn main() {
         lookat,
         vup,
         focus_distance,
-        defocus_angle
+        defocus_angle,
     );
 
     cam.render(world);
